1dab126f53922cabda1ce802e09523159390a706
Ashfaq Ahmed <ashfaqahmed177@gmail.com>
2017-05-16 09:36:26 +0530

    Update myfirstc.c

--- myfirstc.c
+++ myfirstc.c
@@ -4,7 +4,7 @@ void main()
 {
   int a=24;
   printf("Hi This is ashfaq and i am %d years old,a);
-  printf("I thought of taking edit option to check how the edits are saved");
+
   /*This is my first comment on GITHUB*/         
   getch();
 }
